57e4af8d616be487b2f2b7df63f38f579ecd97a1
20055662 <20055662@tafe.wa.edu.au>
2025-04-07 16:50:22 +0800

    created a game program

---
+++ src/guesser.py
@@ -0,0 +1,20 @@
+import random
+
+def play_game():
+    secret_number = random.randint(1, 10)
+    attempts = 0
+
+    print ("Guess  a number between 1 and 10")
+
+    while True:
+        guess = int(input("your guess: "))
+        attempts += 1
+
+        if guess == secret_number:
+            print (f"Correct! it took you {attempts} attempts")
+            break
+        elif guess > secret_number:
+                print ("Too high")
+        else:
+                print ("Too low!")
+
